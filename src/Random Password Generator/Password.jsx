69f502c5e06eb36fa84160c25fd750a9a09fafce
unknown <sadachhena@gmail.com>
2026-02-16 08:59:35 -0800

    CSS Change

--- src/Random Password Generator/Password.jsx
+++ src/Random Password Generator/Password.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Password() {
   return (
     <div>
-      <h1>Rendow password</h1>
+      <h1 className=' font-bold text-4xl text-center'>Random password</h1>
     </div>
   )
 }
